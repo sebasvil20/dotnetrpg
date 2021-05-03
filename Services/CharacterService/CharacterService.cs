6a0304c6c24a3f7390b48e6646485f2ec10e0d5f
Sebastian Villegas <sebasvil20@gmail.com>
2021-05-03 16:20:33 -0500

    Fixing auto mapper issue

--- Services/CharacterService/CharacterService.cs
+++ Services/CharacterService/CharacterService.cs
@@ -18,13 +18,14 @@ namespace dotnet_rpg.Services.CharacterService
         public CharacterService(IMapper mapper)
         {
             _mapper = mapper;
-
         }
 
         public async Task<ServiceResponse<List<GetCharacterDto>>> AddCharacter(AddCharacterDto newCharacter)
         {
             var ServiceResponse = new ServiceResponse<List<GetCharacterDto>>();
-            characters.Add(_mapper.Map<Character>(newCharacter));
+            Character character = _mapper.Map<Character>(newCharacter);
+            character.Id = characters.Max(c => c.Id) + 1;
+            characters.Add(character);
             ServiceResponse.Data = characters.Select(c => _mapper.Map<GetCharacterDto>(c)).ToList();
             return ServiceResponse;
         }
