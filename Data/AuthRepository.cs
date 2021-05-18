209463cceb977f383481d21ddeec2f6fdbea2cb2
Sebastian Villegas <sebasvil20@gmail.com>
2021-05-18 13:10:09 -0500

    adding first authentication steps
    Added the models, Dtos, register and login methods

---
+++ Data/AuthRepository.cs
@@ -0,0 +1,77 @@
+using System.Threading.Tasks;
+using dotnet_rpg.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace dotnet_rpg.Data
+{
+    public class AuthRepository : IAuthRepository
+    {
+        private readonly DataContext _context;
+        public AuthRepository(DataContext context)
+        {
+            _context = context;
+
+        }
+        public async Task<ServiceResponse<string>> Login(string username, string password)
+        {
+            ServiceResponse<string> response = new ServiceResponse<string>();
+            User user = await _context.Users.FirstOrDefaultAsync(x => x.Username.ToLower().Equals(username.ToLower()));
+            if(user == null){
+                response.Sucess = false;
+                response.Message = "User not found";
+            } 
+            else if(!VerifyPasswordHash(password, user.PasswordHash, user.PasswordSalt)){
+                response.Sucess = false;
+                response.Message = "Wrong password";
+            }
+            else{
+                response.Data = user.Id.ToString();
+            }
+            return response;
+        }
+
+        public async Task<ServiceResponse<int>> Register(User user, string password)
+        {
+            
+            ServiceResponse<int> response = new ServiceResponse<int>();
+            if(await UserExists(user.Username)){
+                response.Sucess = false;
+                response.Message = "User already exists";
+                return response;
+            }
+            CreatePasswordHash(password, out byte[] passwordHash, out byte[] passwordSalt);
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+            response.Data = user.Id;
+            return response;
+        }
+
+        public async Task<bool> UserExists(string username)
+        {
+                if(await _context.Users.AnyAsync(x => x.Username.ToLower() == username.ToLower())){
+                    return true;
+                }
+                return false;
+        }
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt){
+            using(var hmac = new System.Security.Cryptography.HMACSHA512()){
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt){
+            using(var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt)){
+                var ComputedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                for(int i = 0; i < ComputedHash.Length; i++){
+                    if(ComputedHash[i] != passwordHash[i]) return false;
+                }
+                return true;
+            }
+        }
+    }
+}
