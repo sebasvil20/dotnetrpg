7ee64089d2df6916cf4e34dfe90444667ff8a799
Sebastian Villegas <sebasvil20@gmail.com>
2021-05-22 10:28:17 -0500

    Updating UpdateCharacter method
    Now only the owner can use it

--- Services/CharacterService/CharacterService.cs
+++ Services/CharacterService/CharacterService.cs
@@ -91,18 +91,27 @@ namespace dotnet_rpg.Services.CharacterService
             var ServiceResponse = new ServiceResponse<GetCharacterDto>();
             try
             {
-                Character character = await _context.Characters.FirstOrDefaultAsync(c => c.Id == updatedCharacter.Id);
+                Character character = await _context.Characters.Include(c => c.User).FirstOrDefaultAsync(c => c.Id == updatedCharacter.Id);
+                if(character.User.Id == GetUserId()){
 
-                character.Name = updatedCharacter.Name;
-                character.HitPoints = updatedCharacter.HitPoints;
-                character.Strength = updatedCharacter.Strength;
-                character.Defense = updatedCharacter.Defense;
-                character.Intelligence = updatedCharacter.Intelligence;
-                character.Class = updatedCharacter.Class;
+                    character.Name = updatedCharacter.Name;
+                    character.HitPoints = updatedCharacter.HitPoints;
+                    character.Strength = updatedCharacter.Strength;
+                    character.Defense = updatedCharacter.Defense;
+                    character.Intelligence = updatedCharacter.Intelligence;
+                    character.Class = updatedCharacter.Class;
 
-                await _context.SaveChangesAsync();
+                    _context.Characters.Update(character);
+                    await _context.SaveChangesAsync();
 
-                ServiceResponse.Data = _mapper.Map<GetCharacterDto>(character);
+                    ServiceResponse.Data = _mapper.Map<GetCharacterDto>(character);
+
+                }
+                else{
+                    ServiceResponse.Sucess = false;
+                    ServiceResponse.Message = "Character not found.";
+                }
+
             }
             catch (Exception ex)
             {
